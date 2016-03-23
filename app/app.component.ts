ab361b135119afc9035db227f3aee66e1b96b036
trevorelvey <student@epicodus.com>
2016-03-23 13:25:59 -0700

    initial files and styling

---
+++ app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from 'angular2/core';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>To-Do List</h1>
+      <h3 *ngFor="#task of tasks" (click) = "taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
+    </div>
+  `
+})
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
+}
